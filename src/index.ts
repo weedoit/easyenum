2c3435a204bde0d3e560dbf6d0a6a39d0f2ca9e5
brunoziie <eu@brunoziie.com>
2024-10-13 21:49:15 -0300

    ft: expose type to infer enum

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 export function EasyEnum<T extends string>(...items: T[]): { [K in T]: K } {
-  return items.reduce((acc, cur) => {
-    acc[cur] = cur;
-    return acc;
-  }, {} as { [K in T]: K });
+  return items.reduce(
+    (acc, cur) => {
+      acc[cur] = cur;
+      return acc;
+    },
+    {} as { [K in T]: K }
+  );
 }
+
+export type InferEnum<T> =
+  T extends ReturnType<typeof EasyEnum<infer U extends string>> ? U : never;
